52d17b01656386f01550748c693ae45ac93e54ca
ArmaanM77 <armaanmahajanbg@gmail.com>
2026-01-29 16:55:04 +0530

    Fixes

--- src/controls/setup.py
+++ src/controls/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'contol_node = controls.control_node:main',
+            'control_node = controls.control_node:main',
         ],
     },
 )
