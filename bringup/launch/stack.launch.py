795613bde523b5f567cd8da487f2ae6844371136
ArmaanM77 <armaanmahajanbg@gmail.com>
2026-02-04 10:30:41 +0530

    Link ros nodes with one file

---
+++ bringup/launch/stack.launch.py
@@ -0,0 +1,64 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    common = [{"use_sim_time": True}]
+
+    return LaunchDescription([
+        # 1) Perception
+        Node(
+            package="perception",
+            executable="lidar_node",
+            name="lidar",
+            output="screen",
+            parameters=common,
+        ),
+        Node(
+            package="perception",
+            executable="camera_node",
+            name="camera",
+            output="screen",
+            parameters=common,
+        ),
+        Node(
+            package="perception",
+            executable="fusion_node",
+            name="fusion",
+            output="screen",
+            parameters=common,
+        ),
+
+        # 2) SLAM
+        Node(
+            package="slam",
+            executable="pred_node",
+            name="pred",
+            output="screen",
+            parameters=common,
+        ),
+        Node(
+            package="slam",
+            executable="slam_node",
+            name="slam",
+            output="screen",
+            parameters=common,
+        ),
+
+        # 3) Path planning
+        Node(
+            package="pathplanning",
+            executable="pp_node_skidpad",
+            name="pp",
+            output="screen",
+            parameters=common,
+        ),
+
+        # # 4) Controls
+        # Node(
+        #     package="control_v2",
+        #     executable="control_loop",
+        #     name="control",
+        #     output="screen",
+        #     parameters=common,
+        # ),
+    ])
---
+++ bringup/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='you',
+    maintainer_email='you@todo.todo',
+    description='Bringup launch files',
+    license='TODO',
+    tests_require=['pytest'],
+    entry_points={'console_scripts': []},
+)
